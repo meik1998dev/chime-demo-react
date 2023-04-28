c369b07ba0710d39a6e9d87927b8af2ad1a97469
meik1998news <Mohammad+test@newsifier.com>
2023-04-28 12:16:41 +0300

    tes

---
+++ Meet.tsx
@@ -0,0 +1,84 @@
+import {
+   DeviceLabels,
+   LocalVideo,
+   MeetingStatus,
+   RemoteVideos,
+   useLocalVideo,
+   useMeetingManager,
+   RosterAttendee,
+   useContentShareControls,
+   ContentShare,
+} from 'amazon-chime-sdk-component-library-react';
+import { MeetingSessionConfiguration } from 'amazon-chime-sdk-js';
+import { useEffect, useState } from 'react';
+
+export const Meet = () => {
+   const meetingManager = useMeetingManager();
+   const [meetingId, setMeetingId] = useState<any>();
+   const [attendee, setAttendee] = useState<any>();
+   const { toggleVideo } = useLocalVideo();
+   const [meetings, setmeetings] = useState<any>([]);
+   const { toggleContentShare } = useContentShareControls();
+
+   const joinMeeting = async () => {
+      const meetingSessionConfiguration = new MeetingSessionConfiguration(
+         meetingId,
+         attendee,
+      );
+
+      // const options = {
+      //    deviceLabels: DeviceLabels.AudioAndVideo,
+      // };
+
+      await meetingManager.join(meetingSessionConfiguration);
+
+      await meetingManager.start();
+   };
+
+   const createMeetingSession = async () => {
+      await fetch('https://chime.axensodev.com/api/meetings/create', {
+         method: 'POST',
+      });
+   };
+
+   const createAtendee = async () => {
+      const res = await fetch(
+         `https://chime.axensodev.com/api/meetings/${meetingId?.uuid}/attendee/create`,
+         { method: 'POST' },
+      );
+      const data = await res.json();
+
+      setAttendee(data.data.attend);
+   };
+
+   useEffect(() => {
+      if (!attendee?.AttendeeId) return;
+      joinMeeting();
+   }, [attendee?.AttendeeId]);
+
+   useEffect(() => {
+      const fetchMeetings = async () => {
+         const res = await fetch(`https://chime.axensodev.com/api/meetings`);
+         const data = await res.json();
+         setmeetings(data.data.meetings);
+      };
+      fetchMeetings();
+   }, []);
+
+   return (
+      <div style={{ minHeight: '100vh' }}>
+         {/* <RosterAttendee attendeeId={attendee?.attendeeId} /> */}
+         <button onClick={createAtendee}>Join</button>
+         <button onClick={toggleVideo}>toggle video</button>
+         <button onClick={createMeetingSession}>create meeting</button>
+         <ContentShare />
+         <button onClick={toggleContentShare}>Toggle content share</button>
+         <div>
+            Meetings list
+            {meetings.map((meet: any) => (
+               <div onClick={() => setMeetingId(meet)}>{meet.uuid}</div>
+            ))}
+         </div>
+      </div>
+   );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import {
+   MeetingProvider,
+   lightTheme,
+   GlobalStyles,
+   VideoTileGrid,
+} from 'amazon-chime-sdk-component-library-react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+   return (
+      <ThemeProvider theme={lightTheme}>
+         <GlobalStyles />
+         <MeetingProvider>
+            <VideoTileGrid noRemoteVideoView={<>No videos</>} />
+
+            <Component {...pageProps} />
+         </MeetingProvider>
+      </ThemeProvider>
+   );
 }
